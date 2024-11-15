1cded1f7b8869cf4230012c936bc64b16c79cf6e
Daniel_Dlugosz <d.dlugosz@storware.eu>
2024-11-15 01:26:05 +0100

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,48 @@
+let slideIndexDesktop = 0;
+let slideIndexMobile = 0;
+
+function plusSlides(n, sliderType) {
+  if (sliderType === 'desktop') {
+    showSlides(slideIndexDesktop += n, 'desktop');
+  } else if (sliderType === 'mobile') {
+    showSlides(slideIndexMobile += n, 'mobile');
+  }
+}
+
+function showSlides(n, sliderType) {
+  let i;
+  let slides = document.querySelectorAll(`.${sliderType}-slider .slide`);
+  if (n >= slides.length) {
+    if (sliderType === 'desktop') {
+      slideIndexDesktop = 0;
+    } else if (sliderType === 'mobile') {
+      slideIndexMobile = 0;
+    }
+  }
+  if (n < 0) {
+    if (sliderType === 'desktop') {
+      slideIndexDesktop = slides.length - 1;
+    } else if (sliderType === 'mobile') {
+      slideIndexMobile = slides.length - 1;
+    }
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  if (sliderType === 'desktop') {
+    slides[slideIndexDesktop].style.display = "block";
+  } else if (sliderType === 'mobile') {
+    slides[slideIndexMobile].style.display = "block";
+  }
+}
+
+// Inicjalizacja slajderów
+document.addEventListener("DOMContentLoaded", () => {
+  showSlides(slideIndexDesktop, 'desktop');
+  showSlides(slideIndexMobile, 'mobile');
+});
+
+document.getElementById('mobile-menu').addEventListener('click', () => {
+  const nav = document.querySelector('.nav-links');
+  nav.classList.toggle('active');
+});
